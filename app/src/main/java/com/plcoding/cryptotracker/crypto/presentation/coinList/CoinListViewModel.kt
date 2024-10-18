56cb1d246c6aa7fcdb342bd5ce66aff4f7e1d3ba
Alexander Pozas <a.posas@outlook.com>
2024-10-17 22:10:43 -0600

    Handle network errors with one time events

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinList/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinList/CoinListViewModel.kt
@@ -6,9 +6,11 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -28,6 +30,11 @@ class CoinListViewModel(
             CoinListState()
         )
 
+    // One time events
+    // From viewmodel to a composable
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
+
     fun onAction(action: CoinListAction) {
         when (action) {
             is CoinListAction.CoinItemClicked -> TODO()
@@ -46,10 +53,11 @@ class CoinListViewModel(
                         )
                     }
                 }
-                .onError { _ ->
+                .onError { networkError ->
                     _state.update { prevState ->
                         prevState.copy(isLoading = false)
                     }
+                    _events.send(CoinListEvent.Error(error = networkError))
                 }
         }
     }

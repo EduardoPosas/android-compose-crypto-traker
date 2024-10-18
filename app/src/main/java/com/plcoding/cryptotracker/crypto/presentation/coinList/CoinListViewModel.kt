bf3722ebc0f58e3f552cb5988fdeb3cb9ceee023
Alexander Pozas <a.posas@outlook.com>
2024-10-17 20:50:51 -0600

    Add coin list viewModel and user Intentions

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coinList/CoinListViewModel.kt
@@ -0,0 +1,50 @@
+package com.plcoding.cryptotracker.crypto.presentation.coinList
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptotracker.core.domain.util.onError
+import com.plcoding.cryptotracker.core.domain.util.onSuccess
+import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state = _state
+        .onStart {
+            loadCoins()
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            CoinListState()
+        )
+
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            coinDataSource.getCoins()
+                .onSuccess { coinList ->
+                    _state.update { prevState ->
+                        prevState.copy(
+                            isLoading = false,
+                            coins = coinList.map { it.toCoinUi() }
+                        )
+                    }
+                }
+                .onError { _ ->
+                    _state.update { prevState ->
+                        prevState.copy(isLoading = false)
+                    }
+                }
+        }
+    }
+}
